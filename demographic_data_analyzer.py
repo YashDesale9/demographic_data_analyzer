8a07afac59db523a5bf15286c3ad0094c646b666
Yash Desale <yashdesale910@gmail.com>
2024-11-07 19:37:56 +0000

    demographic-data-analyzer

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -18,24 +18,24 @@ def calculate_demographic_data(print_data=True):
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    higher_education = df.query("education in ['Bachelors', 'Masters', 'Doctorate']")
+    lower_education = df.query('education not in ["Bachelors", "Masters", "Doctorate"]')
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_education_rich = ((higher_education['salary'].value_counts()['>50K']/higher_education.shape[0])*100).round(1)
+    lower_education_rich = ((lower_education['salary'].value_counts()['>50K']/lower_education.shape[0])*100).round(1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    num_min_workers = df[df['hours-per-week']== min_work_hours]
 
-    rich_percentage = None
+    rich_percentage = ((num_min_workers['salary'].value_counts()['>50K']/num_min_workers.shape[0])*100).round(3)
 
     # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    highest_earning_country = (df[df['salary'] == '>50K']['native-country'].value_counts()).index[0]
+    highest_earning_country_percentage = (((df[df['salary'] == '>50K']['native-country'].value_counts()).max()/(df[df['salary'] == '>50K']['native-country'].value_counts()).sum())*100).round(1)
 
     # Identify the most popular occupation for those who earn >50K in India.
     top_IN_occupation = None
